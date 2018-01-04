02191f183904ad2aaa3e8f389eb3377e27417a09
Scott Jackson <scottrj@umd.edu>
2018-01-04 08:59:56 -0500

    updated with Day 2 homework answers, including some sample data for the purpose of doing the homework

--- day02/day02_homework.R
+++ day02/day02_homework.R
@@ -2,28 +2,44 @@
 # Use comments where you're having trouble or questions
 
 # 1. Read your data set into R
+scottdata <- read.csv("scottdata/CognitionPaperFinalData.csv")
+
 
 # 2. Peek at the top few rows
+head(scottdata)
 
 # 3. Peek at the top few rows for only a few columns
+head(scottdata[, 1:4])
+head(scottdata[, c("Corpus", "Pair", "WithinMinPairs")])
 
 # 4. How many rows does your data have?
+nrow(scottdata)
 
 # 5. Get a summary for every column
+summary(scottdata)
 
 # 6. Get a summary for one column
+summary(scottdata$Corpus)
 
 # 7. Are any of the columns giving you unexpected values?
+# Nope!
 
 # 8. Select a few key columns, make a vector of the column names
+colnames(scottdata)
+key.columns <- colnames(scottdata)[c(1, 3, 6, 8)]
 
 # 9. Create a new data.frame with just that subset of columns
+scottdata.sub1 <- scottdata[, key.columns]
 
 # 10. Create a new data.frame that is just the first 10 rows
 #     and the last 10 rows of the data from the previous step
+scottdata.sub2 <- scottdata.sub1[c(1:10, (nrow(scottdata.sub1)-9):nrow(scottdata.sub1)), ]
+scottdata.sub2
 
 # 11. Create a new data.frame that is a random sample of half of the rows.
 # HINT: ?sample
+sample.rows <- sample(x = nrow(scottdata), size = nrow(scottdata)/2) # replace = FALSE is the default
+sample.rows
 
 # 12. Find a comparison in your data that is interesting to make
 #     (comparing two sets of numbers)
@@ -33,6 +49,14 @@
 #     - run the t.test with BOTH the formula and "vector"
 #       formats, if possible
 #     - if one is NOT possible, say why you can't do it
+my.ttest1 <- t.test(WithinMinPairs ~ Merged, data = scottdata)
+my.ttest1
+
+my.ttest2 <- t.test(BetweenMinPairs ~ Merged, data = scottdata)
+my.ttest2
+
+# currently no good way to just refer to the rows where Merged column is "Merged" or "Unmerged"
+# we'll fix that soon!
 
 # 13. Repeat #12 for TWO more comparisons
 #     - ALTERNATIVELY, if correlations are more interesting,
@@ -40,8 +64,11 @@
 #       Pearson correlations)
 #     - Tip: it's okay if the comparisons are kind of nonsensical, this is 
 #       just a programming exercise
+my.cortest1 <- cor.test(scottdata$WithinMinPairs, scottdata$BetweenMinPairs)
+(my.cortest2 <- cor.test(scottdata$HiSegTok, scottdata$BetweenMinPairs))
 
 # 14. Save all results from #12 and #13 in an .RData file
+save(my.ttest1, my.ttest2, my.cortest1, my.cortest2, file = "day02/homework_results.RData")
 
 # 15. Email me your version of this script, PLUS the .RData
 #     file from #14
